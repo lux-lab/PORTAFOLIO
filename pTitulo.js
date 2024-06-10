0836579fee0d359d06b2d4dd8da189a97f092654
lux-lab <81647606+lux-lab@users.noreply.github.com>
2024-06-09 22:55:09 -0500

    Update pTitulo.js

--- pTitulo.js
+++ pTitulo.js
@@ -8,7 +8,7 @@ var s1 = function ( sketch ) {
 
   sketch.setup = () => {
 
-    let graficaCanvas = sketch.createCanvas(windowWidth, 400, WEBGL);
+    let graficaCanvas = sketch.createCanvas(windowWidth, 200);
 
     graficaCanvas.parent('titulo1');
 
